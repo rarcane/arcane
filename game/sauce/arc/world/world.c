db1cd5a4b3ba31aca89dc13e228785c19859f38b
Thomas Randall <TJRandall27@gmail.com>
2019-11-30 10:09:07 +1000

    BG2 & 3 Pine Trees

--- game/sauce/arc/world/world.c
+++ game/sauce/arc/world/world.c
@@ -48,22 +48,28 @@ internal void TempInitGameWorld()
 		SetupBackgroundEntity(bg1_tree_1, v2(10.0f, -5.0f), STATIC_SPRITE_bg1_pine_tree_v1, 5.2f, v2(0.2f, 0.1f));
 
 		Entity *bg2_hills3 = NewEntity("hills");
-		SetupBackgroundEntity(bg2_hills3, v2(-300.0f, 0.0f), STATIC_SPRITE_bg2_hills_v1, 6.1f, v2(0.35f, 0.2f));
+		SetupBackgroundEntity(bg2_hills3, v2(-300.0f, 0.0f), STATIC_SPRITE_bg2_hills_v1, 6.1f, v2(0.4f, 0.2f));
 		Entity *bg2_hills2 = NewEntity("hills");
-		SetupBackgroundEntity(bg2_hills2, v2(-100.0f, 0.0f), STATIC_SPRITE_bg2_hills_v1, 6.1f, v2(0.35f, 0.2f));
+		SetupBackgroundEntity(bg2_hills2, v2(-100.0f, 0.0f), STATIC_SPRITE_bg2_hills_v1, 6.1f, v2(0.4f, 0.2f));
 		Entity *bg2_hills4 = NewEntity("hills");
-		SetupBackgroundEntity(bg2_hills4, v2(100.0f, 0.0f), STATIC_SPRITE_bg2_hills_v1, 6.0f, v2(0.35f, 0.2f));
+		SetupBackgroundEntity(bg2_hills4, v2(100.0f, 0.0f), STATIC_SPRITE_bg2_hills_v1, 6.0f, v2(0.4f, 0.2f));
 		Entity *bg2_hills1 = NewEntity("hills");
-		SetupBackgroundEntity(bg2_hills1, v2(300.0f, 0.0f), STATIC_SPRITE_bg2_hills_v1, 6.0f, v2(0.35f, 0.2f));
+		SetupBackgroundEntity(bg2_hills1, v2(300.0f, 0.0f), STATIC_SPRITE_bg2_hills_v1, 6.0f, v2(0.4f, 0.2f));
+
+		Entity *bg2_tree_1 = NewEntity("bgtree");
+		SetupBackgroundEntity(bg2_tree_1, v2(50.0f, -5.0f), STATIC_SPRITE_bg2_pine_tree_v1, 6.5f, v2(0.4f, 0.2f));
 
 		Entity *bg3_hills1 = NewEntity("hills");
-		SetupBackgroundEntity(bg3_hills1, v2(100.0f, 0.0f), STATIC_SPRITE_bg3_hills_v1, 7.0f, v2(0.5f, 0.3f));
+		SetupBackgroundEntity(bg3_hills1, v2(100.0f, 0.0f), STATIC_SPRITE_bg3_hills_v1, 7.0f, v2(0.6f, 0.3f));
 		Entity *bg3_hills2 = NewEntity("hills");
-		SetupBackgroundEntity(bg3_hills2, v2(-100.0f, 0.0f), STATIC_SPRITE_bg3_hills_v1, 7.0f, v2(0.5f, 0.3f));
+		SetupBackgroundEntity(bg3_hills2, v2(-100.0f, 0.0f), STATIC_SPRITE_bg3_hills_v1, 7.0f, v2(0.6f, 0.3f));
 		Entity *bg3_hills3 = NewEntity("hills");
-		SetupBackgroundEntity(bg3_hills3, v2(300.0f, 0.0f), STATIC_SPRITE_bg3_hills_v1, 7.0f, v2(0.5f, 0.3f));
+		SetupBackgroundEntity(bg3_hills3, v2(300.0f, 0.0f), STATIC_SPRITE_bg3_hills_v1, 7.0f, v2(0.6f, 0.3f));
 		Entity *bg3_hills4 = NewEntity("hills");
-		SetupBackgroundEntity(bg3_hills4, v2(-300.0f, 0.0f), STATIC_SPRITE_bg3_hills_v1, 7.0f, v2(0.5f, 0.3f));
+		SetupBackgroundEntity(bg3_hills4, v2(-300.0f, 0.0f), STATIC_SPRITE_bg3_hills_v1, 7.0f, v2(0.6f, 0.3f));
+
+		Entity *bg3_tree_1 = NewEntity("bgtree");
+		SetupBackgroundEntity(bg3_tree_1, v2(-20.0f, -5.0f), STATIC_SPRITE_bg3_pine_tree_v1, 7.5f, v2(0.6f, 0.3f));
 	}
 
 	{
