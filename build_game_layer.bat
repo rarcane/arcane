a64e5c425238655a8102baab21c3362b397b425e
Thomas Randall <TJRandall27@gmail.com>
2020-04-03 15:12:36 +0930

    Fixed data desk path

--- build_game_layer.bat
+++ build_game_layer.bat
@@ -16,7 +16,7 @@ rem  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 pushd data_desk_custom
 pushd build
 echo --- Building Data Desk custom layer.
-start /b /wait "" "cl.exe" -nologo /Zi /FC -I../../tools/data_desk/source/ ../source/arcane_data_desk.c /LD
+start /b /wait "" "cl.exe" -nologo /Zi /FC -I../../tools/data_desk/source/ ../../data_desk_custom/source/arcane_data_desk.c /LD
 popd
 popd
 
