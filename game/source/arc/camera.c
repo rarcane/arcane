a3720ea2108d19b66f324830b289e7c5393e0269
Randall <tjrandall27@gmail.com>
2020-07-14 11:40:30 +1000

    Standardised zooming across all resolutions

--- game/source/arc/camera.c
+++ game/source/arc/camera.c
@@ -1,7 +1,7 @@
 static void TransformEditorCamera()
 {
 	f32 transform_mult = platform->key_down[KEY_shift] ? 2.0f : 1.0f;
-
+	
 	if (platform->mouse_scroll_y != 0.0f)
 	{
 		core->camera_zoom += platform->mouse_scroll_y / 120.0f / 4 * transform_mult;
@@ -10,7 +10,7 @@ static void TransformEditorCamera()
 		else if (core->camera_zoom > 10.0f)
 			core->camera_zoom = 10.0f;
 	}
-
+	
 	if (platform->key_down[KEY_a])
 	{
 		core->camera_position.x += 600.0f * transform_mult / core->camera_zoom * core->delta_t;
@@ -32,8 +32,10 @@ static void TransformEditorCamera()
 static void TransformInGameCamera()
 {
 	PositionComponent *position_comp = GetPositionComponentFromEntityID(core->run_data->character_entity->entity_id);
-	core->camera_zoom = DEFAULT_CAMERA_ZOOM;
+	
+	core->camera_zoom = core->render_h / (1080.0f / DEFAULT_CAMERA_ZOOM);
+	
 	core->camera_position = V2AddV2(v2(core->camera_offset.x, core->camera_offset.y + DEFAULT_CAMERA_OFFSET_Y), V2MultiplyF32(position_comp->position, -1.0f));
-
+	
 	core->run_data->disable_chunk_loaded_based_off_view = 0;
 }
