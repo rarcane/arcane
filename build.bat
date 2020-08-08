2749940d652b1de281db50bf5bf21a3fe65af4fb
ryanfleury <ryan.j.fleury@gmail.com>
2020-08-07 18:24:07 -0600

    Remove fastlink

--- build.bat
+++ build.bat
@@ -12,7 +12,7 @@ set application_dll=arcane.dll
 set build_options= -DBUILD_WIN32=1 -DBUILD_DEBUG=1 -DBUILD_RELEASE=0 -DBUILD_DEVELOPER=1 -DROLE=%role%  -DTSFOUNDATION_PROGRAM_OPTIONS=\"program_options.h\"  -DTS2D=1  -DTSASSETS=1  -DTSDEVTERMINAL=1  -DTSINPUT=1  -DTSUI=1 
 set common_compiler_flags= /TC /GL- -MTd -nologo -Gm- -GR- -Gs- -Gy- -Gw- -EHa- -Od -W3 -wd4201 -wd4100 -wd4189 /Zi -D_CRT_SECURE_NO_WARNINGS /FS /FC /I..\source\ %build_options% 
 set platform_linker_flags= user32.lib gdi32.lib winmm.lib 
-set common_linker_flags= -opt:ref -incremental:no /Debug:fastlink opengl32.lib     
+set common_linker_flags= -opt:ref -incremental:no opengl32.lib     
 if not exist game\build mkdir game\build
 pushd game\build
 del *.pdb > NUL 2> NUL
