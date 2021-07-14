343f71b2024ba7a92702802311f941cbb6370331
randy <personal@randy.gg>
2021-07-15 09:19:51 +1000

    big ron now big proper yes

--- game/source/arc/animation.c
+++ game/source/arc/animation.c
@@ -7,9 +7,10 @@ internal void TransformSkeleton(Ts2dSkeleton *skeleton)
 		skeleton->bones[i].parent_index = -1;
 	}
 	
-	for (i32 i = 0; i < core->tsm.meshes[0].bone_count; i++)
+	Mesh *mesh = &core->tsm.meshes[0];
+	for (i32 i = 0; i < mesh->bone_count; i++)
 	{
-		BoneInfo *bone_info = &core->tsm.meshes[0].bone_infos[i];
+		BoneInfo *bone_info = &mesh->bone_infos[i];
 		skeleton->bones[i].parent_index = bone_info->parent_index;
 		
 		m4 transform = M4InitD(1.0f);
@@ -18,8 +19,18 @@ internal void TransformSkeleton(Ts2dSkeleton *skeleton)
 		
 		v3 position = key_frames->key_positions[0].position;
 		
+		if (bone_info == &mesh->bone_infos[mesh->root_bone_index])
+		{
+			position = V3MultiplyF32(position, .001f);
+		}
+		
 		if (platform->key_down[KEY_t])
+		{
+			/*
+						transform = M4MultiplyM4(transform, M4MultiplyM4(mesh->inverse_root_matrix, M4InitD(100.f)));
+			 */
 			transform = M4MultiplyM4(transform, M4TranslateV3(position));
+		}
 		
 		// TODO(randy): Transform actin funni. Translation animation is in some weird space.
 		// The transform on the spine is very large for some reason.
